179c517a130e9ecda1fe4c18fc1be2041613bbe3
John G. Asmussen <86524036+jgasmussen@users.noreply.github.com>
2023-01-21 19:40:18 -0600

    Case_Notes.py Version 1.1 Released.
    Updated to Version 1.1

--- Case_Notes.py
+++ Case_Notes.py
@@ -2,60 +2,43 @@
 
 ######################################################
 #                   Case_Notes.py                    #
-#                    Version 1.0                     #
+#                    Version 1.1                     #
 #----------------------------------------------------#
 #           Written by: John G. Asmussen             #
 #          EGA Technology Specialists, LLC.          #
 #                   GNU GPL v 3.0                    #
 ######################################################
 
+import argparse
 import datetime
 import os
-import sys
 import platform
+import sys
 from pyautogui import hotkey
 
-log_filename = sys.argv[1] + '.log'
-
-if '-h' in sys.argv[1] or '--help' in sys.argv[1]:
-  print("Case_Notes.py: a program for creating a case notes log file complete with UTC date and timestamps.")
-  print('\n')
-  print("Usage: python3 Case_Notes.py [NAME_OF_LOG_FILE] [DATE_TIMESTAMP_FORMAT]")
-  print('\n')
-  print(" [--help] or [-h] prints this message.")
-  print('\n')
-  print(" [--UTC] specifies the UTC date/timestamp for the log file.")
-  print('\n')
-  print(" [--LocalTime] specifies the Local Time date/timestamp for the log file.")
-  print('\n')
-  print("For a new case, the user will be prompted to enter their Name, ID, and a Case Number.")
-  print('\n')
-  print("The program will drop the user to a persistent prompt where case notes can be entered. There are only two command options to use:")
-  print('\n')
-  print("   - Enter !s to take a screenshot.")
-  print('\n')
-  print("   - Enter !q to exit the program.")
-  print('\n')
-  print("The latest version of this script can be found here: https://github.com/jgasmussen/Case_Notes.py")
-  sys.exit()
+parser = argparse.ArgumentParser(description = "Case_Notes.py is a cross-platform (Windows, macOS, & Linux) python script to help make the documentation process easier.", epilog = "The latest version of this script can be found here: https://github.com/jgasmussen/Case_Notes.py")
+parser.add_argument("-f", "--filename", metavar="", required=True, action="store", help="user specified name of the log file.")
+parser.add_argument("-t", "--time", choices=['UTC', 'localtime'], metavar="", required=True, action="store", help="log file date/time format - must choose either 'UTC' or 'localtime'.")
+args = parser.parse_args()
 
 def write_log(log):
-  with open(log_filename, 'a') as log_file:
-    if '--UTC' in sys.argv[2]:
+  with open(args.filename + '.log', 'a') as log_file:
+    if args.time == 'UTC':
       log_file.write(str(datetime.datetime.utcnow()) + "(UTC Time) |: " + log + "\n")
 
-    if '--LocalTime' in sys.argv[2]:
+    if args.time == 'localtime':
       log_file.write(str(datetime.datetime.now()) + "(LocalTime) |: " + log + "\n")
 
-if os.path.exists(log_filename):
+if os.path.exists(args.filename + '.log'): 
   write_log("=============== USER RESTARTED PROGRAM ===============")
 
-if not os.path.exists(log_filename):
+if not os.path.exists(args.filename + '.log'):
+  agency_company = input("Enter Agency / Company name: ")
   examiner_name = input("Enter Examiner's name: ")
   examiner_id = input("Enter Examiner's ID#: ")
   case_number = input("Enter Case#: ")
-  header = "Examiner's name: " + examiner_name + "\n" + "Examiner's ID#: " + examiner_id + "\n" + "Case#: " + case_number + "\n" + "\n"
-  with open(log_filename, 'w') as log_file:
+  header = "Agency / Company name: " + agency_company + "\n" + "Examiner's name: " + examiner_name + "\n" + "Examiner's ID#: " + examiner_id + "\n" + "Case#: " + case_number + "\n" + "\n"
+  with open(args.filename + '.log', 'w') as log_file:
     log_file.write(header)
 
 while True:
